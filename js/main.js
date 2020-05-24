24c9442551d188196cc61245b63da0ed86eaa2bc
Lungu Olga <lunguolga2000@yahoo.com>
2020-05-24 16:31:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+ jQuery(document).ready(function(){
+     
+    "use strict";
+     $('#slider-carousel').carouFredSel({
+         responsive:true,
+         width:'100%',
+         circular:true,
+         scroll:{
+                 items:1,
+                 duration:500,
+                
+             
+         },
+         auto:true,
+         items:
+         {
+             visible:{
+                   min:1,
+                   max:1
+             },
+             height:"variable"
+         },
+         pagination:{
+             container:".sliderpager",
+             pageAnchorBuilder:false
+         }
+         
+     });
+    $(window).scroll(function(){
+       var top = $(window).scrollTop();
+        if(top>=60){
+          $("header").addClass('secondary'); 
+        }
+        else 
+            if($("header").hasClass('secondary')){
+                $("header").removeClass('secondary');  
+            }
+    });
+    
+     
+     
+     
+       $('.animation').each(function(){
+
+		var waypoint = new Waypoint({
+		  element: this,
+		  handler: function(direction) {
+		    var animation = $(this.element).attr('data-animation');
+		    $(this.element).css('opacity','1');
+		    $(this.element).addClass("animated " + animation);
+		  },
+		  offset: '75%' 
+		})
+	});
+ });
+
+
+ 
